281127ecbf163f22625090821660e708e3c6631c
sheliyaharsh2004 <hp0111258@gmail.com>
2022-06-09 09:53:00 +0530

    data

--- src/container/Doctor/Doctor.js
+++ src/container/Doctor/Doctor.js
@@ -4,47 +4,56 @@ import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { DataGrid, renderActionsCell } from "@mui/x-data-grid";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 import { useFormik, Formik, Form } from "formik";
 import * as yup from "yup";
 
 function Doctor(props) {
   const [open, setOpen] = useState(false);
   const [Dopen, setDopen] = useState(false);
-  const [editopen, setEditOpen] = useState(false);
   const [showData, setEShowData] = useState([]);
-  const [did, setdid] = useState('');
-  const [Editdata, setEditdata] = useState([]);
+  const [did, setdid] = useState("");
+  const [udate, setUdate] = useState(false);
 
   const handleClickOpen = () => {
-      setOpen(true);
+    setOpen(true);
   };
 
   const handleClose = () => {
-      setOpen(false);
-      setDopen(false);
-      setEditOpen(false);
+    setOpen(false);
+    setDopen(false);
   };
 
   const handleClickDopen = (id) => {
-      setdid(id)
-      setDopen(true);
+    setdid(id);
+    setDopen(true);
   };
 
-  const handleClickEOpen = (id) => {
-      setEditOpen(true);
-      console.log(id);
-      EditData(id);
+  const handleClickEditOpen = (params) => {
+    console.log(params.row);
+    setdid(params.id);
+    setOpen(true);
+
+    formik.setValues({
+      name: params.row.name,
+      email: params.row.email,
+      sallery: params.row.sallery,
+      post: params.row.post,
+      experience: params.row.experience,
+    });
+    setUdate(true);
   };
 
   let schema = yup.object().shape({
     name: yup.string().required("Please enter name"),
-    email: yup.string().email("Please enter valid name").required("Please enter eamil"),
+    email: yup
+      .string()
+      .email("Please enter valid name")
+      .required("Please enter eamil"),
     sallery: yup.string().required("Please enter sallery"),
     post: yup.string().required("Please enter Post"),
     experience: yup.string().required("Please enter experience"),
@@ -52,37 +61,64 @@ function Doctor(props) {
 
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      sallery: '',
-      post: '',
-      experience:'',
+      name: "",
+      email: "",
+      sallery: "",
+      post: "",
+      experience: "",
     },
     validationSchema: schema,
     onSubmit: (values, { resetForm }) => {
       const { name, email, sallery, post, experience } = values;
 
-      let docdata = {
-        id: Math.floor(Math.random() * 1000),
-        name,
-        email,
-        sallery,
-        post,
-        experience,
-      };
-      let doctorData = JSON.parse(localStorage.getItem("doctor"));
-
-      if (doctorData == null) {
-        localStorage.setItem("doctor", JSON.stringify([docdata]));
+      if (udate) {
+        Updata(values);
       } else {
-        doctorData.push(docdata);
-        localStorage.setItem("doctor", JSON.stringify(doctorData));
+        let docdata = {
+          id: Math.floor(Math.random() * 1000),
+          name,
+          email,
+          sallery,
+          post,
+          experience,
+        };
+        let doctorData = JSON.parse(localStorage.getItem("doctor"));
+
+        if (doctorData == null) {
+          localStorage.setItem("doctor", JSON.stringify([docdata]));
+        } else {
+          doctorData.push(docdata);
+          localStorage.setItem("doctor", JSON.stringify(doctorData));
+        }
+        handleClose();
+        getData();
+        resetForm();
       }
-      handleClose();
-      getData();
-      resetForm();
     },
   });
+
+  const Updata = (values) => {
+    console.log(values);
+
+    let edituData = JSON.parse(localStorage.getItem("doctor"));
+    console.log(edituData);
+
+    let upadateData = edituData.map((u) => {
+      if (u.id === did) {
+        console.log(did);
+        return { id: did, ...values };
+      } else {
+        return u;
+      }
+    });
+
+    localStorage.setItem("doctor", JSON.stringify(upadateData));
+
+    getData();
+    setOpen(false);
+    setUdate(false);
+    setdid();
+  };
 
   const getData = () => {
     let getEDataItem = JSON.parse(localStorage.getItem("doctor"));
@@ -106,13 +142,13 @@ function Doctor(props) {
 
   const EditData = (id) => {
     console.log(id);
-    
+
     let GetEditData = JSON.parse(localStorage.getItem("doctor"));
 
-    let EData = GetEditData.filter((e,i) => e.id == id)
+    let EData = GetEditData.filter((e, i) => e.id == id);
 
     console.log(JSON.stringify(EData));
-  }
+  };
 
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
@@ -130,7 +166,8 @@ function Doctor(props) {
           <>
             <IconButton
               className="border-primary"
-              onClick={() => handleClickDopen(params.id)}>
+              onClick={() => handleClickDopen(params.id)}
+            >
               <DeleteIcon />
             </IconButton>
           </>
@@ -144,9 +181,7 @@ function Doctor(props) {
       renderCell: (params) => {
         return (
           <>
-            <IconButton
-              
-              onClick={() => handleClickEOpen(params.id)}>
+            <IconButton onClick={() => handleClickEditOpen(params)}>
               <EditIcon />
             </IconButton>
           </>
@@ -179,64 +214,79 @@ function Doctor(props) {
                 margin="dense"
                 id="name"
                 name="name"
+                value={formik.values.name}
                 label="Name"
                 type="text"
                 fullWidth
                 variant="standard"
                 onChange={formik.handleChange}
               />
-              {formik.errors.name ? <p className="errors">{formik.errors.name}</p> : null}
+              {formik.errors.name ? (
+                <p className="errors">{formik.errors.name}</p>
+              ) : null}
               <TextField
                 autoFocus
                 margin="dense"
                 id="email"
                 name="email"
+                value={formik.values.email}
                 label="Email"
                 type="email"
                 fullWidth
                 variant="standard"
                 onChange={formik.handleChange}
               />
-              {formik.errors.email ? <p className="errors">{formik.errors.email}</p> : null}
+              {formik.errors.email ? (
+                <p className="errors">{formik.errors.email}</p>
+              ) : null}
               <TextField
                 autoFocus
                 margin="dense"
                 id="sallery"
                 name="sallery"
+                value={formik.values.sallery}
                 label="Sallery"
                 type="text"
                 fullWidth
                 variant="standard"
                 onChange={formik.handleChange}
               />
-              {formik.errors.sallery ? <p className="errors">{formik.errors.sallery}</p> : null}
+              {formik.errors.sallery ? (
+                <p className="errors">{formik.errors.sallery}</p>
+              ) : null}
               <TextField
                 autoFocus
                 margin="dense"
                 id="post"
                 name="post"
                 label="Post"
+                value={formik.values.post}
                 type="text"
                 fullWidth
                 variant="standard"
                 onChange={formik.handleChange}
               />
-              {formik.errors.post ? <p className="error">{formik.errors.post}</p> : null}
+              {formik.errors.post ? (
+                <p className="error">{formik.errors.post}</p>
+              ) : null}
               <TextField
                 autoFocus
                 margin="dense"
                 id="experience"
                 name="experience"
+                value={formik.values.experience}
                 label="Experience"
                 type="text"
                 fullWidth
                 variant="standard"
                 onChange={formik.handleChange}
               />
-              {formik.errors.experience ? <p className="errors">{formik.errors.experience}</p> : null}
+              {formik.errors.experience ? (
+                <p className="errors">{formik.errors.experience}</p>
+              ) : null}
               <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose} type="submit">Submit</Button>
+                <Button type="submit">Submit</Button>
               </DialogActions>
             </DialogContent>
           </Form>
